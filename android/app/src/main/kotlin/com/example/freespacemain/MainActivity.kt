da1a562402110bb9f32d6facdb166c1f20cf3cbd
ghost-87 <Ghost@87>
2021-08-19 20:33:36 +0530

    second commit

---
+++ android/app/src/main/kotlin/com/example/freespacemain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.freespacemain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:freespacemain/src/screens/home/home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
